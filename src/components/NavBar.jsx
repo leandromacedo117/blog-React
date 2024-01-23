afc286c088d1777918474c1032ddd9d910bcdf7e
Leandro Macedo <leeandromacedo117@gmail.com>
2024-01-23 20:21:07 -0300

    blog

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+        <h2>
+            <Link to={'/'}>Blog</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to={'/new'} className='new-post-btn'>Novo post</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
